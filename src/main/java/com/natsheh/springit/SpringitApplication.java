e02d8480fdf1b816a1ec951d7337c039eab1615a
Ahmed Natsheh <ahmednatsheh@gmail.com>
2020-12-09 19:00:58 +0200

    Adding a welcome message.

--- src/main/java/com/natsheh/springit/SpringitApplication.java
+++ src/main/java/com/natsheh/springit/SpringitApplication.java
@@ -1,13 +1,17 @@
 package com.natsheh.springit;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class SpringitApplication {
 
     public static void main(String[] args) {
+        log.debug("Welcome to Springit!");
         SpringApplication.run(SpringitApplication.class, args);
+        System.out.println(("Welcome to Springit!"));
     }
 
 }
