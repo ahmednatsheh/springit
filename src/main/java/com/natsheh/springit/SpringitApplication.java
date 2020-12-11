0dc6ee1dafe78705abe522943585e44aaf742af7
Ahmed Natsheh <ahmednatsheh@gmail.com>
2020-12-11 20:21:22 +0200

    This is the ready master till now.

--- src/main/java/com/natsheh/springit/SpringitApplication.java
+++ src/main/java/com/natsheh/springit/SpringitApplication.java
@@ -1,5 +1,6 @@
 package com.natsheh.springit;
 
+import com.natsheh.springit.config.SpringitProperties;
 import com.natsheh.springit.model.Comment;
 import com.natsheh.springit.model.Link;
 import com.natsheh.springit.repository.CommentRepository;
@@ -8,11 +9,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableConfigurationProperties(SpringitProperties.class)
 @Slf4j
 public class SpringitApplication {
 
